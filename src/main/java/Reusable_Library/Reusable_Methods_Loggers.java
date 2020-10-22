43fd7ab3743ca25b8e6ad00f92303e7d864d7c5c
Atik Howlader <atikhowlader97@gmail.com>
2020-10-22 17:19:01 -0400

    assignment 8

--- src/main/java/Reusable_Library/Reusable_Methods_Loggers.java
+++ src/main/java/Reusable_Library/Reusable_Methods_Loggers.java
@@ -11,12 +11,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 
 public class Reusable_Methods_Loggers {
 
-    static int timeout = 10;
+    static int timeout = 15;
 
     public static WebDriver getDriver() throws IOException, InterruptedException {
         //kill all chrome driver instance before opening a new one
@@ -213,6 +216,69 @@ public class Reusable_Methods_Loggers {
             getScreenShot(driver,logger,elementName);
         }
     }//end of hover method
+
+    public static void uploadFile(String path) throws InterruptedException, AWTException {
+            Thread.sleep(4000);
+
+//File Need to be imported
+
+            File file = new File(path);
+
+            StringSelection stringSelection= new StringSelection(file.getAbsolutePath());
+
+//Copy to clipboard Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+
+            Robot robot = new Robot();
+
+// Cmd + Tab is needed since it launches a Java app and the browser looses focus
+
+            robot.keyPress(KeyEvent.VK_META);
+
+            robot.keyPress(KeyEvent.VK_TAB);
+
+            robot.keyRelease(KeyEvent.VK_META);
+
+            robot.keyRelease(KeyEvent.VK_TAB);
+
+            robot.delay(500);
+
+//Open Goto window
+
+            robot.keyPress(KeyEvent.VK_META);
+
+            robot.keyPress(KeyEvent.VK_SHIFT);
+
+            robot.keyPress(KeyEvent.VK_G);
+
+            robot.keyRelease(KeyEvent.VK_META);
+
+            robot.keyRelease(KeyEvent.VK_SHIFT);
+
+            robot.keyRelease(KeyEvent.VK_G);
+
+//Paste the clipboard value
+
+            robot.keyPress(KeyEvent.VK_META);
+
+            robot.keyPress(KeyEvent.VK_V);
+
+            robot.keyRelease(KeyEvent.VK_META);
+
+            robot.keyRelease(KeyEvent.VK_V);
+
+//Press Enter key to close the Goto window and Upload window
+
+            robot.keyPress(KeyEvent.VK_ENTER);
+
+            robot.keyRelease(KeyEvent.VK_ENTER);
+
+            robot.delay(500);
+
+            robot.keyPress(KeyEvent.VK_ENTER);
+
+            robot.keyRelease(KeyEvent.VK_ENTER);
+
+        }// end of method
 
 }//end of java class
 
---
+++ src/test/java/Action_Items/Assignment_8.java
@@ -0,0 +1,47 @@
+package Action_Items;
+
+import Reusable_Library.Abstract_Class_Cross_Browser;
+import Reusable_Library.Reusable_Methods_Loggers;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+
+public class Assignment_8 extends Abstract_Class_Cross_Browser {
+
+    @Test
+    public void fedexInfo () throws InterruptedException, AWTException {
+        driver.manage().deleteAllCookies();
+        //navigate to express homepage
+        driver.navigate().to("https://www.fedex.com/en-us/home.html");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        Reusable_Methods_Loggers.click(driver,"//*[@aria-label='Open Design & Print menu']",logger,"Design and Print");
+        Reusable_Methods_Loggers.click(driver,"//*[@alt='Upload a Print-Ready File']",logger,"Upload a Print Ready File");
+        String actualTitle = driver.getTitle();
+        if (actualTitle.contains("Fedex Office Print")){
+            System.out.println(actualTitle);
+            logger.log(LogStatus.PASS,"The page title is " + actualTitle);
+        } else {
+            System.out.println("The actual title is " + actualTitle);
+            logger.log(LogStatus.FAIL,"The actual page title is " + actualTitle);
+        }//end of if else condition
+        Reusable_Methods_Loggers.clickByIndex(driver,"//*[@class='js-selectProjectType']",2,logger,"Document Printing");
+        Reusable_Methods_Loggers.click(driver,"//*[@title='Copies & Custom Projects']",logger,"Copies & Custom Projects");
+        Reusable_Methods_Loggers.click(driver,"//*[@name='upload-files-button']",logger,"Use your File");
+        Thread.sleep(4000);
+        Reusable_Methods_Loggers.uploadFile("/Users/atikhowlader/Documents/Indianapolis-Colts1.jpg");
+        Thread.sleep(5000);
+        Reusable_Methods_Loggers.click(driver,"//*[@id='js-modal__continue-btn_lowResolutionPreview']",logger,"Continue");
+        Reusable_Methods_Loggers.sendKeys(driver,"//*[@class='quantity-field']","5",logger,"Quantity");
+        Reusable_Methods_Loggers.click(driver,"//*[@name='update_quanity-addToCart-lbl']",logger,"Add to Cart");
+        Reusable_Methods_Loggers.click(driver,"//*[@id='cartToCheckoutBtn']",logger,"Checkout");
+        Reusable_Methods_Loggers.click(driver,"//*[@id='continueAsGuest']",logger,"Continue as Guest");
+        Reusable_Methods_Loggers.dropDownMenu(driver,"//*[@id='radiusSelect']","5",logger,"Radius");
+        Reusable_Methods_Loggers.typeAndSubmit(driver,"//*[@id='loc_seacrh_text']",logger,"11435","Zipcode");
+        String address = Reusable_Methods_Loggers.captureResult(driver,"//*[@class='active']",logger,"Address");
+        System.out.println(address);
+        logger.log(LogStatus.PASS,"The address is " + address);
+    }//end of test
+
+}//end of class
